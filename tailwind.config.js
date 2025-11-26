cffc58159870479729b39187d22237f0bcba2f7b
backupausing <backupausing@gmail.com>
2025-11-26 18:20:23 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        ionian: "#1F2937",
-        sand: "#EDE8D0",
-        sage: "#A7C4A0",
-        terracotta: "#C57F6B",
-        cream: "#F9F6EF"
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
